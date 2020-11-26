d0b9d665da0d0afdb192340235718da3d2fbd5c9
necdetozil <necdet.ozil@hacettepe.edu.tr>
2020-11-26 20:44:25 +0300

    Navigation is working now.

--- app/src/main/java/com/alperenozil/animalsapp/view/ListFragment.java
+++ app/src/main/java/com/alperenozil/animalsapp/view/ListFragment.java
@@ -2,16 +2,26 @@ package com.alperenozil.animalsapp.view;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavDirections;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.alperenozil.animalsapp.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class ListFragment extends Fragment {
 
+    @BindView(R.id.listFAB)
+    FloatingActionButton listFAB;
 
     public ListFragment() {
     }
@@ -19,6 +29,17 @@ public class ListFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_list, container, false);
+        View view = inflater.inflate(R.layout.fragment_list, container, false);
+        ButterKnife.bind(this,view);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        listFAB.setOnClickListener(v -> {
+            NavDirections action = ListFragmentDirections.actionListFragmentToDetailFragment();
+            Navigation.findNavController(v).navigate(action);
+        });
     }
 }
