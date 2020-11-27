84df7e4621e9137decb8e4d48433719813049e28
necdetozil <necdet.ozil@hacettepe.edu.tr>
2020-11-27 19:42:47 +0300

    Recyclerview is now working.

---
+++ app/src/main/java/com/alperenozil/animalsapp/view/AnimalListAdapter.java
@@ -0,0 +1,44 @@
+package com.alperenozil.animalsapp.view;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.alperenozil.animalsapp.R;
+import com.alperenozil.animalsapp.model.AnimalModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AnimalListAdapter extends RecyclerView.Adapter<AnimalViewHolder> {
+    private ArrayList<AnimalModel> animalList=new ArrayList<>();
+    public void updateAnimalList(List<AnimalModel> newAnimalList){
+        animalList.clear();
+        animalList.addAll(newAnimalList);
+        notifyDataSetChanged();
+    }
+    @NonNull
+    @Override
+    public AnimalViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater=LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.item_animal,parent,false);
+        return new AnimalViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AnimalViewHolder holder, int position) {
+        ImageView imageView=holder.itemView.findViewById(R.id.imageView);
+        TextView textView=holder.itemView.findViewById(R.id.textView);
+        textView.setText(animalList.get(position).name);
+    }
+
+    @Override
+    public int getItemCount() {
+        return animalList.size();
+    }
+}
---
+++ app/src/main/java/com/alperenozil/animalsapp/view/AnimalViewHolder.java
@@ -0,0 +1,12 @@
+package com.alperenozil.animalsapp.view;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class AnimalViewHolder extends RecyclerView.ViewHolder {
+    public AnimalViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
